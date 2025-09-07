e67ec665b093e2870163320c68ea78e2cbd5e5db
Sergio Velarde Álvarez <sergio.velarde@alumno.uneatlantico.es>
2025-09-07 16:33:11 +0200

    Back del buscador y funcionalidad seguimientos

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Tymon\JWTAuth\Contracts\JWTSubject;
 
+
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use App\Models\Publication;
